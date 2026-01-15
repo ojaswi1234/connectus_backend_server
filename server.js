f2faa4002208aeffb1680e069d7d59d5883e9521
Ojaswi <ojaswideep2020@gmail.com>
2026-01-16 04:33:44 +0530

    fixed port problem

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ useServer({
   }
 }, wsServer);
 
-server.listen(4000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('Server is running on http://localhost:4000/graphql');
 });
